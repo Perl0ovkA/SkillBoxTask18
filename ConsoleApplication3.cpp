23e5f4ccadd6ad5b51d59fe2af9b46a83683ffe8
Perl0ovkA <104366771+Perl0ovkA@users.noreply.github.com>
2024-07-18 19:56:43 +0300

    1

--- ConsoleApplication3.cpp
+++ ConsoleApplication3.cpp
@@ -1,44 +1,99 @@
 ﻿#include <iostream>
 #include <math.h>
 #include <algorithm>
+#include <string>
 using namespace std;
 
 class Player
 {
+private:
+	string NamePlay;
+	int ScorePlay;
 public:
-	Player()
+	Player() : NamePlay("Name"), ScorePlay(0)
+	{}
+	Player(string _NamePlay, int _ScorePlay) : NamePlay(_NamePlay), ScorePlay(_ScorePlay)
+	{}
+    void Show()
 	{
-		name = "default";
-		score = 0;
+		cout << "Player name: " << NamePlay << "Player score: " << ScorePlay << endl;
 	}
-	Player(string N, int S)
+	
+	int GetScore()
 	{
-		name = N;
-		score = S;
+		return ScorePlay;
 	}
 
-	int score;
-	string name;
-
-	void Show()
+	void SetScore(int newScore)
 	{
-		cout << "Player name: " << name << "Player score: " << score << endl;
+		ScorePlay = newScore;
+	}
+	string GetNamePlay()
+	{
+		return NamePlay;
+	}
+
+	void GetName(string newNamePlay)
+	{
+		NamePlay = newNamePlay;
+	}
+
+	void setPlay(string newNamePlay, int newScore)
+	{
+		NamePlay = newNamePlay;
+		ScorePlay = newScore;
 	}
 };
+
+void SelectionSort(Player* pl, int size)
+{
+	int j;
+	Player tmp;
+	for (int i = 0; i < size; i++)
+	{
+		j = i;
+		for (int k = i; k < size; k++)
+		{
+			if (pl[j].GetScore() > pl[k].GetScore())
+			{
+				j = k;
+			}
+		}
+		tmp = pl[i];
+		pl[i] = pl[j];
+		pl[j] = tmp;
+	}
+}
 
 int main()
 {
 	int size;
 	cout << "Enter the number of players: ";
 	cin >> size;
+
+	string arrName;
+	int arrScore;
 	Player* pl = new Player[size];
+	for (int i = 0, j = 0; i < size; i++, j++)
+	{
+		string y;
+		int x;
+		cout << "Enter Player: ";
+		cin >> y;
+		arrName = y;
+		cout << "Enter Score: ";
+		cin >> x;
+		arrScore = x;
+
+		pl[i].setPlay(arrName, arrScore);
+	}
+
+	cout << "\nYou enter: \n";
 	for (int i = 0; i < size; i++)
 	{
-		cout << "Enter Player: ";
-		cin >> pl[i].name;
-		cout << "Enter Score: ";
-		cin >> pl[i].score;
-		sort(pl[i].score, pl[i].score + size);
 		pl[i].Show();
 	}
+
+	SelectionSort(pl, size);
+	cout << endl;
 }
